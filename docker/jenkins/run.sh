9e990b8c235504c40f3498f464db8f52b39c347b
laihui0207 <laihui0207gm@gmail.com>
2015-04-09 00:59:05 +0800

    change jenkis run shell

--- docker/jenkins/run.sh
+++ docker/jenkins/run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-#-Dcom.sun.akuma.Daemon=daemonized --daemon
-exec /usr/java/bin/java  -Djava.awt.headless=true -DJENKINS_HOME=/opt/jenkins/home -jar /usr/lib/jenkins/jenkins.war --logfile=/opt/jenkins/logs/jenkins.log --webroot=/var/cache/jenkins/war  --httpPort=8080 --ajp13Port=8009 --debug=2 --handlerCountMax=500 --handlerCountMaxIdle=50 --prefix=/jenkins
+service sshd start
+service jenkins start
+
+function stop_svc {
+service jenkins stop
+service sshd stop
+exit
+}
+trap 'stop_svc' SIGTERM
+while true
+do
+	sleep 1
+done
