2462090c2c39ba19ce32af74007eb02305117de3
laihui0207 <laihui0207gm@gmail.com>
2015-04-14 07:29:20 +0000

    change tomcat docker file

--- docker/tomcat/run.sh
+++ docker/tomcat/run.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 service sshd start
 source /etc/profile
-/opt/apache-tomcat_1/bin/catalina.sh run 
+/opt/apache-tomcat_1/bin/startup.sh
+
+function stop_svc {
+#/opt/apache-tomcat_1/bin/shutdown.sh
+pid=`ps -efww | grep "[f]ile=/opt/apache-tomcat_${i}" | awk '{ print $2 }'`
+kill -9 $pid >/dev/null 2>&1
+service sshd stop
+exit
+}
+trap 'stop_svc' SIGTERM
+while true
+do
+	sleep 1
+done
